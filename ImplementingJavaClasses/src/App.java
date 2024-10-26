1f03cb68d59d7772d2134301b7053fd7a3ecb708
Erick Nyakundi <ericknyakundi4@gmail.com>
2024-10-26 16:23:20 +0300

    update on implementing ImplementingJavaClasses

--- ImplementingJavaClasses/src/App.java
+++ ImplementingJavaClasses/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Account myAccount = new Account(12345, "Erick Nyakundi", 0.0);
+        myAccount.deposit(10000);
+        System.out.println("Balance: " + myAccount.getBalance());
+
+        myAccount.withdraw(5000);
+        System.out.println("Balance: " + myAccount.getBalance());
     }
 }
